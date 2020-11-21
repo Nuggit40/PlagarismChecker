0add5cd6919aff28d4dd0cb0f0219313b3755a14
Sreenath Yeragudi <65571010+SreenathYeragudi@users.noreply.github.com>
2020-11-21 14:03:00 -0500

    Update tokenizer.c

--- tokenizer.c
+++ tokenizer.c
@@ -5,6 +5,10 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include "fileList.h"
+#include <ctype.h>
+
+
 char* readInFile(char* fileName)
 {
    char* buffer = (char* )(malloc(sizeof(char)));
@@ -34,10 +38,66 @@ char* readInFile(char* fileName)
     char*dne = "DNE\0";
     return dne;
 }
-int main(int argc, char **argv){
-    char* f=readInFile(argv[1]);
-    puts(f);
+wordNode* newNode(char* value, int i){   
+    wordNode *res=malloc(sizeof(wordNode)); 
+    res->text=value;   
+    res->occurrence=i;
+	res->next=NULL;
+    return res;
+}
+void printList(wordNode *head){   
+    wordNode *ptr=head;   
 
+    while(ptr!=NULL){   
+        printf("%s-->",ptr->text);   
+        ptr=ptr->next;    
+        
+    }
+    printf("\n");
+}
+void end(wordNode *head)
+{
+    wordNode *p,*q;
+    p=malloc(sizeof(wordNode));
+    p->next=NULL;
+    q=head;
+    while(q->next!=NULL)
+    {
+        q = q->next;
+    }
+    q->next = p;
+}
+void word_tok(char *input){
+	char* buff = (char*)malloc(sizeof(char));
+	int i = 0, inputLength = strlen(input);
+	wordNode *ptr=(wordNode *)malloc(sizeof(wordNode *));
+	wordNode *head=(wordNode *)malloc(sizeof(wordNode *));
+	wordNode *headOG=(wordNode *)malloc(sizeof(wordNode *));
+	while( i < inputLength){
+		while(isspace(input[i]) || input[i]=='\n' && i < inputLength){
+			i++;
+		}
+		if (i >= inputLength){
+			break;
+		}
+		if(isalnum(input[i])){
+			strcpy(buff, "");
+			while(isalnum(input[i]) || input[i] == '-'){
+				strncat(buff, input + i, 1);
+				i++;
+			}
+		}
+        head=newNode(buff,1); 
+        end(head);
+        printList(head);
+	}
+}
+
+int main(int argc, char **argv){
+	char* f = (char*)malloc(sizeof(char));
+    f=readInFile(argv[1]);
+	word_tok(f);
+ 
 }
 
    
